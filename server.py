f42570daa2529e840c1f4ab196616d80d8060ecc
Manuel Napoli <napolimanuel@gmail.com>
2021-10-28 11:42:12 -0300

    Server edited

--- server.py
+++ server.py
@@ -24,10 +24,10 @@ def handle_info():
     print("INFO")
     return {
         "apiversion": "1",
-        "author": "",  # TODO: Your Battlesnake Username
-        "color": "#888888",  # TODO: Personalize
-        "head": "default",  # TODO: Personalize
-        "tail": "default",  # TODO: Personalize
+        "author": "manunapo",
+        "color": "#FF0080",
+        "head": "evil",
+        "tail": "fat-rattle",
     }
 
 
